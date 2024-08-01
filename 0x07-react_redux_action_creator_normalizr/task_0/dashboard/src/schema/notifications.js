8adbc4205d5f9183ecf8789c0ea2402d32e07fca
Selamawit Birhanu Tabor <99592520+Selam25-30@users.noreply.github.com>
2024-07-31 18:29:47 -1200

    Delete 0x07-react_redux_action_creator_normalizr directory

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,6 +0,0 @@
-import * as notifications from '../../../../notifications.json';
-
-export function getAllNotificationsByUser(userId) {
-  const selectedNots = notifications.default.filter((notif) => notif.author.id === userId);
-  return selectedNots.map((notif => notif.context));
-}
--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionTypes.js
+++
@@ -1,6 +0,0 @@
-export const MARK_AS_READ = 'MARK_AS_READ';
-export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
-export const NotificationTypeFilters = {
-  DEFAULT: 'DEFAULT',
-  URGENT: 'URGENT'
-};
